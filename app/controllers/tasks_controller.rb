82b60aef108633208c87b52f4179a552d98611ab
yuta.hirata <engineer.yuta.hirata@gmail.com>
2018-10-09 12:57:16 +0000

    kadai-lesson9-section11

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
-
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -24,11 +24,9 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     if @task.update(task_params)
       flash[:success] = "タスクを更新しました"
       redirect_to @task
@@ -39,7 +37,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     if @task.destroy
       flash[:success] = "タスクを削除しました"
       redirect_to root_url
@@ -50,8 +47,12 @@ class TasksController < ApplicationController
   end
   
   private
+    def set_task
+      @task = Task.find(params[:id])   
+    end
+  
     def task_params
-      params.require(:task).permit(:content)
+      params.require(:task).permit(:content, :status)
     end
   
 end
